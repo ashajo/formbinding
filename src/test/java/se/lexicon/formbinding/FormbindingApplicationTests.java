55a485ef7bf865f31ed5e07dfbb92884b21d9897
asha joseph <ashajo183@gmail.com>
2021-04-23 10:34:48 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/formbinding/FormbindingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.formbinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormbindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
