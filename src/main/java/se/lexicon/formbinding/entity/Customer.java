b09bbdbfea0a2dc9298938ac2355a195801b2f3a
asha joseph <ashajo183@gmail.com>
2021-04-23 11:39:33 +0200

    Converted classes to entities

--- src/main/java/se/lexicon/formbinding/entity/Customer.java
+++ src/main/java/se/lexicon/formbinding/entity/Customer.java
@@ -1,13 +1,26 @@
 package se.lexicon.formbinding.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 @Data
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(nullable = false)
     private String customerId;
+    @Column(nullable = false)
     private String email;
+    @CreationTimestamp
+    @Column(nullable = false,columnDefinition = "TIMESTAMP default CURRENT_TIMESTAMP")
     private LocalDate regDate;
+    @Column(columnDefinition = "tinyint(1) default 1")
     private boolean active;
+    @OneToOne(fetch = FetchType.EAGER,cascade = {CascadeType.ALL})
     private CustomerDetails customerDetails;
 }
